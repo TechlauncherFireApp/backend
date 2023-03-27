58569a2c0b99e24fe2828a17b79e9b3a4779fd10
Xinyun Zhou <Xinyun-Zhou@outlook.com>
2023-03-27 16:56:04 +1100

    finish update the data

--- repository/diet_repository.py
+++ repository/diet_repository.py
@@ -1,5 +1,4 @@
 from domain.entity.diet_requirement import DietRequirement
-from domain.type.dietary import DietaryRequirements
 
 
 def save_dietary_requirements(session, user_id, dietary_requirements):
@@ -9,50 +8,30 @@ def save_dietary_requirements(session, user_id, dietary_requirements):
         # Create a new Diet object if none exists
         diet = DietRequirement(user_id=user_id)
 
-    print(diet)
     # Set the values for the relevant attributes based on the dietary requirements
     for restriction in dietary_requirements.restrictions:
-        if restriction.key == 'halal':
-            diet = DietRequirement(halal=1)
-            # diet.halal = True
-        elif restriction.key == 'vegetarian':
-            diet = DietRequirement(vegetarian=1)
-            # diet.vegetarian = True
-        elif restriction.key == 'vegan':
-            diet = DietRequirement(vegan=True)
-            # diet.vegan = True
-        elif restriction.key == 'nut_allergy':
-            diet = DietRequirement(nut_allergy=True)
-            # diet.nut_allergy = True
-        elif restriction.key == 'shellfish_allergy':
-            diet = DietRequirement(shellfish_allergy=True)
-            # diet.shellfish_allergy = True
-        elif restriction.key == 'gluten_intolerance':
-            diet = DietRequirement(gluten_intolerance=True)
-            # diet.gluten_intolerance = True
-        elif restriction.key == 'kosher':
-            diet = DietRequirement(kosher=True)
-            # diet.kosher = True
-        elif restriction.key == 'lactose_intolerance':
-            diet = DietRequirement(lactose_intolerance=True)
-            # diet.lactose_intolerance = True
-        elif restriction.key == 'diabetic':
-            diet = DietRequirement(diabetic=True)
-            # diet.diabetic = True
-        elif restriction.key == 'egg_allergy':
-            diet = DietRequirement(egg_allergy=True)
-            # diet.egg_allergy = True
+        if restriction.key["key"] == 'halal':
+            diet.halal = True
+        elif restriction.key["key"] == 'vegetarian':
+            diet.vegetarian = True
+        elif restriction.key["key"] == 'vegan':
+            diet.vegan = True
+        elif restriction.key["key"] == 'nut_allergy':
+            diet.nut_allergy = True
+        elif restriction.key["key"] == 'shellfish_allergy':
+            diet.shellfish_allergy = True
+        elif restriction.key["key"] == 'gluten_intolerance':
+            diet.gluten_intolerance = True
+        elif restriction.key["key"] == 'kosher':
+            diet.kosher = True
+        elif restriction.key["key"] == 'lactose_intolerance':
+            diet.lactose_intolerance = True
+        elif restriction.key["key"] == 'diabetic':
+            diet.diabetic = True
+        elif restriction.key["key"] == 'egg_allergy':
+            diet.egg_allergy = True
     if dietary_requirements.custom_restrictions != "":
-        # diet = DietRequirement(other=dietary_requirements.custom_restrictions)
         diet.other = dietary_requirements.custom_restrictions
     session.add(diet)
     session.flush()
     return diet
-
-
-# def from_dietary_requirements(diet_args):
-#     dic_diet = DietaryRequirements
-#     for arg in vars(diet_args):
-#         if getattr(diet_args, arg) == 'halal':
-#
-#     return dic_diet
