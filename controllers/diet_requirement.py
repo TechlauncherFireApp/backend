a2e3f9a58f2400c421d00ab7d8e6711e0e582c83
Xinyun Zhou <Xinyun-Zhou@outlook.com>
2023-03-27 16:56:04 +1100

    finish update the data

--- controllers/diet_requirement.py
+++ controllers/diet_requirement.py
@@ -1,30 +1,11 @@
 from flask import Blueprint, request
 from flask_restful import reqparse, fields, Resource, marshal_with, Api
-from typing import List
 
 from domain import session_scope
-from domain.entity.diet_requirement import DietRequirement
-from domain.type.dietary import DietaryRequirements, DietaryRestriction
+from domain.type.dietary import DietaryRestriction
 from repository.diet_repository import save_dietary_requirements
 
-# id_parser = reqparse.RequestParser()
-# id_parser.add_argument('user_id', type=int, required=True)
-
-# diet_parser = reqparse.RequestParser()
-# diet_parser.add_argument('halal', type=bool, required=False)
-# diet_parser.add_argument('vegetarian', type=bool, required=False)
-# diet_parser.add_argument('vegan', type=bool, required=False)
-# diet_parser.add_argument('nut_allergy', type=bool, required=False)
-# diet_parser.add_argument('shellfish_allergy', type=bool, required=False)
-# diet_parser.add_argument('gluten_intolerance', type=bool, required=False)
-# diet_parser.add_argument('kosher', type=bool, required=False)
-# diet_parser.add_argument('lactose_intolerance', type=bool, required=False)
-# diet_parser.add_argument('diabetic', type=bool, required=False)
-# diet_parser.add_argument('egg_allergy', type=bool, required=False)
-# diet_parser.add_argument('other', type=str, required=True)
-
 new_parser = reqparse.RequestParser()
-# new_parser.add_argument('dietary_requirements', type=DietaryRequirements)
 new_parser.add_argument('user_id', type=int, required=True)
 new_parser.add_argument('restrictions', type=DietaryRestriction, action='append')
 new_parser.add_argument('custom_restrictions', type=str)
@@ -42,8 +23,6 @@ class GetDietaryRequirement(Resource):
         user_id = args['user_id']
 
         diet_args = new_parser.parse_args()
-        print(diet_args)
-        # dic_diet = from_dietary_requirements(diet_args)
 
         with session_scope() as session:
             return {'result': save_dietary_requirements(session, user_id, diet_args)}
