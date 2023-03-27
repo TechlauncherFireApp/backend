6a45dced04ab0001bbd7e04637c1a78c61534300
Xinyun Zhou <Xinyun-Zhou@outlook.com>
2023-03-27 16:11:56 +1100

    able to update

--- repository/diet_repository.py
+++ repository/diet_repository.py
@@ -11,29 +11,31 @@ def save_dietary_requirements(session, user_id, dietary_requirements: DietaryReq
 
     print(diet)
     # Set the values for the relevant attributes based on the dietary requirements
-    # for restriction in dietary_requirements.restrictions:
-    #     if restriction.key == 'halal':
-    #         diet.halal = True
-    #     elif restriction.key == 'vegetarian':
-    #         diet.vegetarian = True
-    #     elif restriction.key == 'vegan':
-    #         diet.vegan = True
-    #     elif restriction.key == 'nut_allergy':
-    #         diet.nut_allergy = True
-    #     elif restriction.key == 'shellfish_allergy':
-    #         diet.shellfish_allergy = True
-    #     elif restriction.key == 'gluten_intolerance':
-    #         diet.gluten_intolerance = True
-    #     elif restriction.key == 'kosher':
-    #         diet.kosher = True
-    #     elif restriction.key == 'lactose_intolerance':
-    #         diet.lactose_intolerance = True
-    #     elif restriction.key == 'diabetic':
-    #         diet.diabetic = True
-    #     elif restriction.key == 'egg_allergy':
-    #         diet.egg_allergy = True
-    #     elif restriction.key == 'other':
-    #         diet.other = dietary_requirements.custom_restrictions
+    for restriction in dietary_requirements.restrictions:
+        if restriction.key == 'halal':
+            diet.halal = True
+        elif restriction.key == 'vegetarian':
+            diet.vegetarian = True
+        elif restriction.key == 'vegan':
+            diet.vegan = True
+        elif restriction.key == 'nut_allergy':
+            diet.nut_allergy = True
+        elif restriction.key == 'shellfish_allergy':
+            diet.shellfish_allergy = True
+        elif restriction.key == 'gluten_intolerance':
+            diet.gluten_intolerance = True
+        elif restriction.key == 'kosher':
+            diet.kosher = True
+        elif restriction.key == 'lactose_intolerance':
+            diet.lactose_intolerance = True
+        elif restriction.key == 'diabetic':
+            diet.diabetic = True
+        elif restriction.key == 'egg_allergy':
+            diet.egg_allergy = True
+        elif restriction.key == 'other':
+            diet.other = dietary_requirements.custom_restrictions
+        session.add(diet)
+        session.flush()
     return diet
 
 
