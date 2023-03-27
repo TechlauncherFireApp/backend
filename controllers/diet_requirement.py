c50a41d00bb072f2cbbc213f0acdf6551187ff2d
Xinyun Zhou <Xinyun-Zhou@outlook.com>
2023-03-27 22:38:30 +1100

    add comments

--- controllers/diet_requirement.py
+++ controllers/diet_requirement.py
@@ -5,29 +5,36 @@ from domain import session_scope
 from domain.type.dietary import DietaryRestriction
 from repository.diet_repository import save_dietary_requirements
 
+# getting the data from the frontend
 new_parser = reqparse.RequestParser()
 new_parser.add_argument('user_id', type=int, required=True)
 new_parser.add_argument('restrictions', type=DietaryRestriction, action='append')
 new_parser.add_argument('custom_restrictions', type=str)
 
+# the result for whether updating the data success
 result_fields = {
     "result": fields.Boolean
 }
 
 
 class GetDietaryRequirement(Resource):
+    """
+    This is a class to store the data of dietary requirement to the database
+    """
     @marshal_with(result_fields)
     def post(self):
+        """
+        Returns:
+            True if the data is updated; False if the data is unable to upload
+        """
+        # using try and except to replace the error message to return false
         try:
             request.get_json(force=True)
             args = new_parser.parse_args()
-            
             user_id = args['user_id']
 
-            diet_args = new_parser.parse_args()
-
             with session_scope() as session:
-                return {'result': save_dietary_requirements(session, user_id, diet_args)}
+                return {'result': save_dietary_requirements(session, user_id, args)}
         except Exception as e:
             return {'result': False}, 400
 
