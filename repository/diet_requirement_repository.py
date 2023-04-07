32c6a856783732fa09945359953bf866d0a661d6
Jiakang Jin <126942292+090997@users.noreply.github.com>
2023-04-07 19:06:32 +1000

    Update diet_requirement_repository.py
    fix the error

--- repository/diet_requirement_repository.py
+++ repository/diet_requirement_repository.py
@@ -2,6 +2,7 @@ from domain.entity.diet_requirement import DietRequirement
 
 
 def get_dietary_requirements(session, user_id):
-
     diet_requirement = session.query(DietRequirement).filter(DietRequirement.user_id == user_id).first()
+    if diet_requirement:
+        session.expunge(diet_requirement)
     return diet_requirement
