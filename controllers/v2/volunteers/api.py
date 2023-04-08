e9cac0b5ef58eb7b7f6cf411008e6907f5e4e8a8
Xinyun Zhou <Xinyun-Zhou@outlook.com>
2023-04-08 23:16:04 +1000

    first edit supervisor authority

--- controllers/v2/volunteers/api.py
+++ controllers/v2/volunteers/api.py
@@ -4,6 +4,7 @@ from controllers.v2 import volunteer_personal_info
 from controllers.v2.v2_blueprint import v2_info_api
 from domain import session_scope
 from repository.volunteer_repository import get_volunteer_info
+from services.jwk import has_role
 
 parser = reqparse.RequestParser()
 parser.add_argument('ID', action='store', type=str)
@@ -11,6 +12,7 @@ parser.add_argument('ID', action='store', type=str)
 
 class VolunteerInfo(Resource):
 
+    @has_role()
     @marshal_with(volunteer_personal_info)
     def get(self):
         args = parser.parse_args()
