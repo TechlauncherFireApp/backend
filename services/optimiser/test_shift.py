90a928e08e10a1dea4dd2db6339b787aa5cb0978
Xinyun Zhou <Xinyun-Zhou@outlook.com>
2023-04-24 16:06:44 +1000

    try testing

---
+++ services/optimiser/test_shift.py
@@ -0,0 +1,43 @@
+import pytest as pytest
+
+from domain.base import Session, Engine
+from services.optimiser.input_processing import *
+
+
+@pytest.fixture
+def session():
+    new_session = Session(bind=Engine)
+    yield new_session
+
+    new_session.close()
+
+
+@pytest.fixture
+def request_id():
+    return '322'
+
+
+def test_get_input_qualrequirements(session, request_id):
+    print(get_input_qualrequirements(session, request_id))
+
+
+def test_get_input_rolerequirements(session, request_id):
+    print(get_input_rolerequirements(session, request_id))
+
+
+def test_get_input_posrequirements(session, request_id):
+    print(get_input_posrequirements(session, request_id))
+
+
+def test_get_input_qualability(session):
+    print(get_input_qualability(session))
+
+
+def test_get_input_roleability(session):
+    print(get_input_roleability(session))
+
+
+def test_get_input_availability(session, request_id):
+    print(get_input_availability(session, request_id))
+
+
