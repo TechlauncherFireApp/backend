782594b5b5652aaa5c6e5ae7d9f21943af04505c
Xinyun Zhou <Xinyun-Zhou@outlook.com>
2023-04-25 18:16:59 +1000

    make some change

--- services/authentication.py
+++ services/authentication.py
@@ -82,7 +82,9 @@ class AuthenticationService():
         if not passwordService.compare(password, user.password):
             return LoginResult.FAIL, None, None
 
-        return LoginResult.SUCCESS, jwk_service.generate(user.id, user.email), user
+        print("role:" +user.role)
+
+        return LoginResult.SUCCESS, jwk_service.generate(user.id, user.email, user.role), user
 
     @staticmethod
     def generate_code(code_len: int):
