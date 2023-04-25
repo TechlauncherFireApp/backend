28bf2b5f2f1cf2d078d920c7bcd92f20c32a881c
Xinyun Zhou <Xinyun-Zhou@outlook.com>
2023-04-25 23:09:19 +1000

    test the authority for the supervisor

--- controllers/v2/volunteer/api.py
+++ controllers/v2/volunteer/api.py
@@ -4,7 +4,7 @@ from controllers.v2.v2_blueprint import v2_bp, v2_api
 from controllers.v2.volunteer.response_models import volunteer_personal_info
 from domain import session_scope, UserType
 from repository.volunteer_repository import get_volunteer_info
-from services.jwk import has_role
+from services.jwk import has_role, requires_auth
 
 parser = reqparse.RequestParser()
 parser.add_argument('user_id', action='store', type=str)
@@ -18,6 +18,7 @@ class VolunteerInfo(Resource):
 
     @marshal_with(volunteer_personal_info)
     @has_role(UserType.ROOT_ADMIN)
+    @requires_auth
     def get(self):
         args = parser.parse_args()
         if args["user_id"] is None:
