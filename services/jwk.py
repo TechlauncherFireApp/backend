9318dcd2cb59f8709b99a121266c590e5bb679c7
Xinyun Zhou <Xinyun-Zhou@outlook.com>
2023-04-30 22:31:53 +1000

    final testing and remove print item

--- services/jwk.py
+++ services/jwk.py
@@ -93,10 +93,6 @@ def has_role(*roles):
         jwkservice = JWKService()
 
         def wrapper(*args, **kwargs):
-            print(**kwargs)
-            print(kwargs)
-            print(*args)
-            print(args[0].__dict__)
             authorization_header = request.headers.get("Authorization")
             if authorization_header is None:
                 # TODO: Throw an error
