ccb562f6671c3d87187cebd69f979d23a139e165
jinjiakang <u7339664@anu.edu.au>
2023-05-01 16:36:34 +1000

    Refactor-RetrieveDietaryRequirement

--- services/optimiser/input_processing.py
+++ services/optimiser/input_processing.py
@@ -350,7 +350,7 @@ def test_vehicle_list(session, request_id):
    for v in v_l:
      get_position_list(session, v)
 
-    #print("get_postion_role", get_position_role(session,720))
+    # print("get_postion_role", get_position_role(session,720))
     # print("get_APR_matrix", get_input_rolerequirements(session,361))
     # get_position_qualification(session,756)
     # print("get_APQ_matrix",get_input_qualrequirements(session,361))
