19ff7c3d2debb12283c8ffe4530b515d7ba8aa46
Xinyun Zhou <Xinyun-Zhou@outlook.com>
2023-05-02 22:00:54 +1000

    add comments

--- services/jwk.py
+++ services/jwk.py
@@ -98,6 +98,7 @@ def has_role(*roles):
         def wrapper(*args, **kwargs):
             authorization_header = request.headers.get("Authorization")
             if authorization_header is None:
+                # TODO: Throw an error
                 pass
             token = authorization_header[len('Bearer '):]
             if jwkservice.validate(token) and jwkservice.validate_role(token, roles):
