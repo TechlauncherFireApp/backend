4ca004f95536af8f599a50b701476506e6049b51
Xinyun Zhou <Xinyun-Zhou@outlook.com>
2023-08-10 13:17:55 +1000

    Update docker

--- services/mail.py
+++ services/mail.py
@@ -8,7 +8,7 @@ from sendgrid.helpers.mail import Mail
 from services.attachment import AttachmentService
 from services.secrets import SecretService
 
-secret = SecretService(f"email/{os.environ.get('env', 'dev')}/api")
+# secret = SecretService(f"email/{os.environ.get('env', 'dev')}/api")
 attachment = AttachmentService()
 
 
@@ -21,39 +21,41 @@ class MailSender:
     }
 
     def __init__(self, **kwargs):
-        self.api_key = secret.get()['api_key']
-        self.from_email = kwargs.get('from_email', secret.get()['from_email'])
+        pass
+        # self.api_key = secret.get()['api_key']
+        # self.from_email = kwargs.get('from_email', secret.get()['from_email'])
 
     def email(self, to_email, template, data, start_time, end_time):
+        pass
         # We always append on the destination url to help.
-        data['url'] = secret.get()['url']
-
-        # Build the mail object & send the email.
-        message = Mail(
-            from_email=self.from_email,
-            to_emails=to_email,
-            html_content='<strong>This will be replaced by your template.</strong>')
-        message.dynamic_template_data = data
-        message.template_id = self.templates[template]
-
-        if template == 'roster':
-            ics = attachment.generate('Shift Assignment', start_time, end_time)
-            message.add_attachment(
-                Attachment(
-                    file_content=base64.b64encode(str(ics).encode('utf-8')).decode(),
-                    file_name='shift.ics',
-                    file_type='text/calendar',
-                    disposition='attachment'
-                ))
-
-        try:
-            sendgrid_client = SendGridAPIClient(api_key=self.api_key)
-            response = sendgrid_client.send(message)
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
-        except Exception as e:
-            print(e)
+        # data['url'] = secret.get()['url']
+        #
+        # # Build the mail object & send the email.
+        # message = Mail(
+        #     from_email=self.from_email,
+        #     to_emails=to_email,
+        #     html_content='<strong>This will be replaced by your template.</strong>')
+        # message.dynamic_template_data = data
+        # message.template_id = self.templates[template]
+        #
+        # if template == 'roster':
+        #     ics = attachment.generate('Shift Assignment', start_time, end_time)
+        #     message.add_attachment(
+        #         Attachment(
+        #             file_content=base64.b64encode(str(ics).encode('utf-8')).decode(),
+        #             file_name='shift.ics',
+        #             file_type='text/calendar',
+        #             disposition='attachment'
+        #         ))
+        #
+        # try:
+        #     sendgrid_client = SendGridAPIClient(api_key=self.api_key)
+        #     response = sendgrid_client.send(message)
+        #     print(response.status_code)
+        #     print(response.body)
+        #     print(response.headers)
+        # except Exception as e:
+        #     print(e)
 
 
 #sender = MailSender()
