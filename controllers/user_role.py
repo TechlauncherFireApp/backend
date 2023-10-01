304d3c1da111229a8c1332c38c66a8929e118912
Ye Tian <u7563012@anu.edu.au>
2023-10-01 16:15:05 +1100

    Update user_role.py for role checked endpoints
    GET: required (admin)
    
    POST: required (admin)
    
    PATCH: required (admin)

--- controllers/user_role.py
+++ controllers/user_role.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 from flask_restful import fields, Resource, marshal_with, Api, reqparse
 
-from domain import session_scope
+from domain import session_scope, UserType
 from repository.user_role_repository import get_user_roles, add_user_role, delete_user_role
 
-from services.jwk import requires_auth
+from services.jwk import requires_auth, has_role
 
 get_fields = {
     'userId': fields.Integer,
@@ -26,12 +26,14 @@ parser.add_argument('roleId', action='store', type=str)
 
 class UserRole(Resource):
     @requires_auth
+    @has_role(UserType.ROOT_ADMIN)
     @marshal_with(get_fields)
     def get(self):
         with session_scope() as session:
             return get_user_roles(session)
 
     @requires_auth
+    @has_role(UserType.ROOT_ADMIN)
     @marshal_with(post_fields)
     def post(self):
         args = parser.parse_args()
@@ -41,6 +43,7 @@ class UserRole(Resource):
             role_id = add_user_role(session, args['userId'], args['roleId'])
             return {'id': role_id}
 
+    @has_role(UserType.ROOT_ADMIN)
     @marshal_with(patch_fields)
     def patch(self):
         args = parser.parse_args()
