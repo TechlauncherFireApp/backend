90ed4fb3b1039391b1baaa2868da9f759fce7d52
Tai Ha <taihavn1101@gmail.com>
2024-03-09 23:39:54 +1100

    refactor application.py to use the standard factory method

--- application.py
+++ application.py
@@ -1,56 +1,51 @@
 from flask import Flask
 from flask_cors import CORS
-
 from controllers import *
 
-# Register the application
 
-application = app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-# TODO: Tech Debt
-#   - CORS Should be specified at the host level per environment, not a global free-for-all. We do this to stop
-#     cross site scripting (XSS) attacks.
-CORS(app)
+    CORS(app)
 
-# Register all controllers individually
-app.register_blueprint(existing_requests_bp)
-app.register_blueprint(new_request_bp)
-app.register_blueprint(recommendation_bp)
-app.register_blueprint(shift_request_bp)
-app.register_blueprint(vehicle_request_bp)
-app.register_blueprint(volunteer_bp)
-app.register_blueprint(volunteer_all_bp)
-app.register_blueprint(volunteer_availability_bp)
-app.register_blueprint(volunteer_preferred_hours_bp)
-app.register_blueprint(volunteer_shifts_bp)
-app.register_blueprint(volunteer_status_bp)
-app.register_blueprint(authentication_bp)
-app.register_blueprint(reference_bp)
-app.register_blueprint(user_role_bp)
-app.register_blueprint(asset_type_role_bp)
-app.register_blueprint(user_type_bp)
-app.register_blueprint(tenancy_config_bp)
-app.register_blueprint(tutorial_quiz_bp)
-app.register_blueprint(email_bp)
-app.register_blueprint(profile_bp)
-app.register_blueprint(v2_bp)
-app.register_blueprint(volunteer_unavailability_bp)
-app.register_blueprint(user_bp)
-app.register_blueprint(chatbot_bp)
-app.register_blueprint(diet_requirement_retrieval_bp)
+    app.register_blueprint(existing_requests_bp)
+    app.register_blueprint(new_request_bp)
+    app.register_blueprint(recommendation_bp)
+    app.register_blueprint(shift_request_bp)
+    app.register_blueprint(vehicle_request_bp)
+    app.register_blueprint(volunteer_bp)
+    app.register_blueprint(volunteer_all_bp)
+    app.register_blueprint(volunteer_availability_bp)
+    app.register_blueprint(volunteer_preferred_hours_bp)
+    app.register_blueprint(volunteer_shifts_bp)
+    app.register_blueprint(volunteer_status_bp)
+    app.register_blueprint(authentication_bp)
+    app.register_blueprint(reference_bp)
+    app.register_blueprint(user_role_bp)
+    app.register_blueprint(asset_type_role_bp)
+    app.register_blueprint(user_type_bp)
+    app.register_blueprint(tenancy_config_bp)
+    app.register_blueprint(tutorial_quiz_bp)
+    app.register_blueprint(email_bp)
+    app.register_blueprint(profile_bp)
+    app.register_blueprint(v2_bp)
+    app.register_blueprint(volunteer_unavailability_bp)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(chatbot_bp)
+    app.register_blueprint(diet_requirement_retrieval_bp)
 
+    @app.route('/')
+    def main():
+        return {
+            'status': 'OK',
+        }
 
-@app.route('/')
-def main():
-    return {
-        'status': 'OK',
-    }
+    return app
 
 
 if __name__ == '__main__':
     import logging
 
     logging.basicConfig(filename='error.log', level=logging.DEBUG)
+    app = create_app()
     app.run(host='0.0.0.0')
-
-
