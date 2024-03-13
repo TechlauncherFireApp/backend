d538db1a9b9d3629a802c477462c9f245ecba216
Tai Ha <97883232+TaiHaDev@users.noreply.github.com>
2024-03-13 11:45:37 +1100

    Johnha/feature/create unavailability testing (#174)
    Setting up a testing environment for the project using pytest. Creating
    testing folders for functional and unit testing. Needed to be further
    refined to have development, testing, and production configuration.
    Currently, testing will create and get directly from the database which
    is not a good practice. Creating some test cases for creating
    unavailability endpoint with testing criteria:
    - two consecutive create operations must output two consecutive event_id
    - Cannot create two unavailability with the same time interval
    - Cannot create availability for nonexistent user_id
    - Cannot create unavailability with an end time earlier than the start
    time
    - Cannot create unavailability with weird periodicity (to be discussed)
    - There cannot be two unavailability from the same user with overlapped
    time interval
    - Merge interval (if implemented on API)
    
    Some significant changes:
    - Creating a tests folder that will contain all future functional and
    unit tests for the project
    - Refactoring application.py class to make use of factory method design
    pattern so that many instances of the application can be created, for
    now, it can be used in testing
    <img width="467" alt="image"
    src="https://github.com/TechlauncherFireApp/backend/assets/97883232/f9ee0b5a-ad7a-469c-8d4e-fbea15e3f0f9">
    
    https://fireapp-emergiq-2024.atlassian.net/jira/software/projects/FE/boards/1?selectedIssue=FE-68

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,20 +5,35 @@ os.environ.setdefault('password', 'password')
 os.environ.setdefault('host', '127.0.0.1')
 os.environ.setdefault('port', '3306')
 os.environ.setdefault('dbname', 'db')
-from application import create_app
 import pytest
+from application import app
 
 
 @pytest.fixture(scope='module')
 def test_client():
-    print("hello")
-
     # Set the Testing configuration prior to creating the Flask application
-    flask_app = create_app()
 
-    with flask_app.test_client() as testing_client:
+    with app.test_client() as testing_client:
         # Establish an application context
-        with flask_app.app_context():
+        with app.app_context():
             yield testing_client
 
 
+@pytest.fixture(scope='module')
+def auth_token(test_client):
+    # Login with predefined admin credentials
+    login_payload = {  # admin account details
+        'email': 'admin',
+        'password': 'admin'
+    }
+    response = test_client.post('/authentication/login', json=login_payload)
+    assert response.status_code == 200, "Failed to log in with the given credential"
+    print(response.json)
+    token = response.json['access_token']
+    return token
+
+
+# append jwt token to header of all the testing requests
+@pytest.fixture(autouse=True)
+def set_auth_header(test_client, auth_token):
+    test_client.environ_base['HTTP_AUTHORIZATION'] = f'Bearer {auth_token}'
