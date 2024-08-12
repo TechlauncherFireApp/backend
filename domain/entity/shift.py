38909f571736562bdebc4324d0e47ce3d1ab0372
YiranLI <lyr060614@163.com>
2024-08-12 16:25:00 +1000

    modified shift entity

--- domain/entity/shift.py
+++ domain/entity/shift.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, UniqueConstraint, Enum
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, UniqueConstraint, Enum, PrimaryKeyConstraint
 from datetime import datetime
 from domain import Base
 from domain.type import UserType
@@ -6,10 +6,12 @@ from domain.type import UserType
 
 class Shift(Base):
     __tablename__ = 'shift'
-    shiftId = Column(Integer, primary_key=True, autoincrement=True)
+    shiftId = Column(Integer, nullable=False)
     userId = Column(Integer, ForeignKey('user.id'), nullable=False)
     title = Column(String(256), nullable=True, default=None)
     start = Column(DateTime, nullable=False, default=datetime.now())
     end = Column(DateTime, nullable=False, default=datetime.now())
     role = Column(Enum(UserType), name='user_type', nullable=False)
-    UniqueConstraint(shiftId, userId, name='event')
+    __table_args__ = (
+        PrimaryKeyConstraint('shiftId', 'userId'),
+    )
