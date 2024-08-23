30cbe3ac0651baa5ffaaae06f4174535af209649
YiranLI <lyr060614@163.com>
2024-08-23 16:01:54 +1000

    modified the shift_request domain

--- domain/entity/shift_request_volunteer.py
+++ domain/entity/shift_request_volunteer.py
@@ -10,9 +10,9 @@ class ShiftRequestVolunteer(Base):
     __tablename__ = 'shift_request_volunteer'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('user.id'), name='user_id')
-    request_id = Column(Integer, ForeignKey('shift_request.id'), name='request_id')
-    status = Column(String(256), name='status')
+    user_id = Column(Integer, ForeignKey('user.id'), name='user_id', nullable=False)
+    request_id = Column(Integer, ForeignKey('shift_request.id'), name='request_id', nullable=False)
+    status = Column(String(12), name='status', nullable=False, default='pending')
     update_date_time = Column(DateTime, name='last_update_datetime', default=datetime.now(), nullable=False)
     insert_date_time = Column(DateTime, name='created_datetime', default=datetime.now(), nullable=False)
 
