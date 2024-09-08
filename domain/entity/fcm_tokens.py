2c19389693f1efca74e1a2bbe2da4a98dca8d3d3
anannnchim <nachimchim@gmail.com>
2024-09-08 13:41:34 +0700

    Add FCMToken domain

---
+++ domain/entity/fcm_tokens.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from sqlalchemy import Column, String, ForeignKey, Integer, Boolean, TIMESTAMP
+from sqlalchemy.orm import relationship
+from domain.base import Base
+
+
+class FCMToken(Base):
+
+    __tablename__ = 'fcm_tokens'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('user.id'), name='user_id', nullable=False)
+    fcm_token = Column(String(255), name='fcm_token', nullable=False)
+    device_type = Column(String(50), name='device_type', nullable=False)
+    created_at = Column(TIMESTAMP, name='created_at', default=datetime.now(), nullable=False)
+    updated_at = Column(TIMESTAMP, name='updated_at', default=datetime.now(), onupdate=datetime.now, nullable=False)
+    is_active = Column(Boolean, name='is_active', default=True, nullable=False)
+
+    user = relationship("User")
