7c78ea212a37fc82cd2a7bfb2f5bd83af6b55196
Steven (Quoc) <u7108792@anu.edu.au>
2024-09-26 18:08:04 +1000

    Revert "added link from specific role to specific shift to match requirements"
    This reverts commit 38746ee489242a7a25a7cabaa96b982f15e1c5a6.

--- domain/entity/role.py
+++ domain/entity/role.py
@@ -1,12 +1,11 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from domain.base import Base
 
 
 class Role(Base):
     __tablename__ = 'role'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    shift_request_id = Column(Integer, ForeignKey('shift_request.id'), name='shift_request_id', nullable=False)
     code = Column(String(256), nullable=False)
     name = Column(String(256), nullable=False)
     deleted = Column(Boolean, nullable=False, default=False)
