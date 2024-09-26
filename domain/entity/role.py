38746ee489242a7a25a7cabaa96b982f15e1c5a6
Steven (Quoc) <u7108792@anu.edu.au>
2024-09-26 17:41:19 +1000

    added link from specific role to specific shift to match requirements

--- domain/entity/role.py
+++ domain/entity/role.py
@@ -1,11 +1,12 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from domain.base import Base
 
 
 class Role(Base):
     __tablename__ = 'role'
     id = Column(Integer, primary_key=True, autoincrement=True)
+    shift_request_id = Column(Integer, ForeignKey('shift_request.id'), name='shift_request_id', nullable=False)
     code = Column(String(256), nullable=False)
     name = Column(String(256), nullable=False)
     deleted = Column(Boolean, nullable=False, default=False)
