8a38f4116a727ff01fa705fdbd72fc67de2e8f69
anannnchim <nachimchim@gmail.com>
2024-10-05 12:57:17 +0700

    Add NotificationService

---
+++ services/notification_service.py
@@ -0,0 +1,43 @@
+import logging
+import os
+import firebase_admin
+from firebase_admin import credentials, messaging, exceptions
+from typing import List, Optional
+
+
+class NotificationService:
+
+    def __init__(self):
+
+        if not firebase_admin._apps:
+            cred_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS', r"C:\Users\User\backend\google-credentials.json")
+            cred = credentials.Certificate(cred_path)
+            firebase_admin.initialize_app(cred)
+
+    def send_notification(self, fcm_token_list: List[str], title: str, body: str, data: Optional[dict] = None) -> None:
+        """
+        Send message to the user device given a list of FCM tokens.
+
+        :param fcm_token_list: List of FCM tokens to send the notification to.
+        :param title: The title of the notification.
+        :param body: The body content of the notification.
+        :param data: Optional additional data for the notification.
+        """
+        if not fcm_token_list:
+            logging.warning("No FCM tokens provided. Cannot send notification.")
+
+        for token in fcm_token_list:
+
+            message = messaging.Message(
+                notification=messaging.Notification(title=title, body=body),
+                data=data or {},
+                token=token
+            )
+
+            try:
+                response = messaging.send(message)
+                logging.info(f"Successfully sent message to token: {token}")
+            except exceptions.FirebaseError as e:
+                logging.error(f"Firebase error for token {token}: {e}")
+            except Exception as e:
+                logging.error(f"Error sending message to token {token}: {e}")
